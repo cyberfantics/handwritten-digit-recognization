000693909cfbbc0b9dd73cbd0b9462e95d3ae5c7
Cyber Fantics <147060613+cyberfantics@users.noreply.github.com>
2024-09-19 17:18:23 +0500

    Remove cv2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import load_model
 from PIL import Image
-import cv2
+
 
 # Load MNIST dataset for reference
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
